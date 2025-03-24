cee888acaf37ec6ab67e87d815058f087efc99c8
Jan Potocnik <jpotoc3@gmail.com>
2025-03-24 14:03:28 +0100

    Implemented the fuctionalities of functions

--- Vaja 2 - BinaryRadixSort.cpp
+++ Vaja 2 - BinaryRadixSort.cpp
@@ -1,23 +1,52 @@
 #include <vector>
 #include <iostream>
 
-void binaryRadixSort(std::vector<unsigned char>& arr)
+void binaryRadixSort(std::vector<unsigned char>& arr) 
 {
-
+    for (int bit = 0; bit < 8; ++bit) 
+    {
+        countingSortByBit(arr, bit);
+    }
 }
 
-void countingSortByBit(std::vector<unsigned char>& arr, int bit)
+void countingSortByBit(std::vector<unsigned char>& arr, int bit) 
 {
+    std::vector<unsigned char> output(arr.size());
+    int count[2] = { 0 };
 
+    for (unsigned char num : arr) 
+    {
+        count[(num >> bit) & 1]++;
+    }
+    count[1] += count[0];
+
+    for (int i = arr.size() - 1; i >= 0; --i) 
+    {
+        output[--count[(arr[i] >> bit) & 1]] = arr[i];
+    }
+
+    arr = output;
 }
 
-void printArray(const std::vector<unsigned char>& arr)
+void printArray(const std::vector<unsigned char>& arr) 
 {
-
+    for (unsigned char num : arr) 
+    {
+        std::cout << (int)num << " ";
+    }
+    std::cout << std::endl;
 }
+
 
 int main()
 {
+    std::vector<unsigned char> arr = { 14, 2, 5, 12 };
+    std::cout << "Original array: ";
+    printArray(arr);
 
+    binaryRadixSort(arr);
+
+    std::cout << "Sorted array: ";
+    printArray(arr);
     return 0;
 }
